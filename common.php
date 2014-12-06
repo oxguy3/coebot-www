36883fee41568b56efede18ddc3308fecfaef32f
Hayden Schiff <oxguy3@gmail.com>
2014-12-06 06:11:15 -0500

    added coebot icon

--- common.php
+++ common.php
@@ -70,7 +70,12 @@ function printNav() {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
 	  </button>
-      <a class="navbar-brand" href="/"><?php echo $SITE_TITLE; ?></a>
+      <a class="navbar-brand" href="/">
+        <span class="pull-left">
+          <img alt="Coebot" src="/img/coebot-icon.png">
+          <?php echo $SITE_TITLE; ?>
+        </span>
+      </a>
     </div>
 <!--     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
