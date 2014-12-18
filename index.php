9c8fd74fccd58f04ccabe42c447ac2928811763b
Hayden Schiff <oxguy3@gmail.com>
2014-12-18 02:03:22 -0500

    cleaner temporary homepage

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 
 require_once("common.php");
 
-printHead("Home"/*, array("/css/chanlist.css"), array("/js/chanlist.js")*/);
+printHead("Home", array("/css/index.css")/*, array("/js/chanlist.js")*/);
 printNav();
 
 ?>
-<div class="container-fluid">
+<div class="container">
   <div class="row">
-    <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-      <h2>Welcome to the beta!</h2>
-      <p>We are working hard on making Coebot's new website. However, the new home page is still being designed! In the mean time, these other pages probably have whatever you might be looking for:</p>
-      <ul>
-        <li><a href="/channels">Channels</a></li>
-        <li><a href="/commands">Commands</a></li>
-      </ul>
+    <div class="col-sm-12">
+      <div class="jumbotron" id="home_placeholder">
+        <h1>Welcome to the beta!</h1>
+        <p>We are working hard on making Coebot's new website. However, the new home page is still being designed! In the mean time, these other pages probably have whatever you might be looking for:</p>
+        <p>
+          <a class="btn btn-primary btn-lg" href="/channels" role="button">Channels</a>
+          <a class="btn btn-primary btn-lg" href="/commands" role="button">Commands</a>
+        </p>
+      </div>
     </div>
   </div>
 </div>
