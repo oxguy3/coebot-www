ecf286be8d4cbd9c4d56cb494cf14c867a52db0f
Hayden Schiff <oxguy3@gmail.com>
2014-12-18 03:32:39 -0500

    more visual improvements to home mockup

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ printNav();
   <div class="row">
 
     <div class="col-md-8 col-lg-9">
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <div id="carousel-whoslive" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <!-- <ol class="carousel-indicators">
           <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -44,7 +44,7 @@ printNav();
             </a>
             <div class="carousel-caption">
               <h3>Coestar</h3>
-              <p><a href="http://twitch.tv/coestar" style="color:inherit">Space Enginerds #StreamADay (Day 378)</a></p>
+              <p><a href="http://twitch.tv/coestar">Space Enginerds #StreamADay (Day 378)</a></p>
             </div>
           </div>
           <div class="item">
@@ -53,7 +53,7 @@ printNav();
             </a>
             <div class="carousel-caption">
               <h3>CoeBot</h3>
-              <p><a href="http://twitch.tv/coebot" style="color:inherit">This is a placeholder status</a></p>
+              <p><a href="http://twitch.tv/coebot">This is a placeholder status</a></p>
             </div>
           </div>
           <div class="item">
@@ -62,29 +62,29 @@ printNav();
             </a>
             <div class="carousel-caption">
               <h3>endsgamer</h3>
-              <p><a href="http://twitch.tv/endsgamer" style="color:inherit">Get back to work, ends.</a></p>
+              <p><a href="http://twitch.tv/endsgamer">Get back to work, ends.</a></p>
             </div>
           </div>
         </div>
 
         <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <a class="left carousel-control" href="#carousel-whoslive" role="button" data-slide="prev">
           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <a class="right carousel-control" href="#carousel-whoslive" role="button" data-slide="next">
           <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
       </div>
     </div>
 
-    <div class="col-md-4 col-lg-3">
-      <h4 style="margin-top:0">Who's live?</h4>
+    <div class="col-md-4 col-lg-3 whoslive-sidebar">
+      <h4>Who's live?</h4>
       <div class="list-group">
-        <a href="#carousel-example-generic" data-slide-to="0" class="list-group-item">Coestar</a>
-        <a href="#carousel-example-generic" data-slide-to="1" class="list-group-item">CoeBot</a>
-        <a href="#carousel-example-generic" data-slide-to="2" class="list-group-item">endsgamer</a>
+        <a href="#carousel-whoslive" data-slide-to="0" class="list-group-item">Coestar</a>
+        <a href="#carousel-whoslive" data-slide-to="1" class="list-group-item">CoeBot</a>
+        <a href="#carousel-whoslive" data-slide-to="2" class="list-group-item">endsgamer</a>
       </ol>
     </div>
   </div>
@@ -93,20 +93,31 @@ printNav();
 
   <div class="row">
 
-    <div class="col-md-4">
+    <div class="col-md-8 col-sm-12 infobox">
+      <h3>Welcome to Coebot</h3>
+      <p class="lead">Coebot is a chat bot for Twitch.tv. Coebot can moderate chat, manage custom commands, track highlights, and much more!</p>
+    </div>
+
+    <div class="col-md-4 col-sm-6 infobox">
       <h3>Need help?</h3>
       <p>The commands list has all the information you need to learn to use Coebot like a pro.</p>
       <p><a class="btn btn-default" href="/commands" role="button">View commands &raquo;</a></p>
     </div>
 
-    <div class="col-md-4">
+    <div class="col-md-4 col-sm-6 infobox">
       <h3>Looking for someone?</h3>
       <p>If you're looking for information about a channel that isn't currently live, you can find them on the channels list.</p>
       <p><a class="btn btn-default" href="/channels" role="button">View channels &raquo;</a></p>
     </div>
 
-    <div class="col-md-4">
-      <h3>Open source</h3>
+    <div class="col-md-4 col-sm-6 infobox">
+      <h3>Do you stream?</h3>
+      <p>If you're a streamer and you want to use Coebot on your channel, drop us a line! We are always happy to welcome new streamers to the Coebot family.</p>
+      <p><a class="btn btn-default" href="https://twitter.com/endsgamer" role="button">Tweet @endsgamer &raquo;</a></p>
+    </div>
+
+    <div class="col-md-4 col-sm-6 infobox">
+      <h3>We're open source!</h3>
       <p>Interested in how Coebot works? All our code is freely licensed, so go ahead and explore!</p>
       <p><a class="btn btn-default" href="https://bitbucket.org/tucker_gardner/coebot" role="button">Bot source &raquo;</a> <a class="btn btn-default" href="https://github.com/oxguy3/coebot-www" role="button">Website source &raquo;</a></p>
     </div>
